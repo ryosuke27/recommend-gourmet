70f485635bd845cef6477ea8540d5ca3471907d6
{ryosuke27} <opu.7455@gmail.com>
2021-05-31 16:40:14 +0900

    fix

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -17,4 +17,26 @@
         </div>
     </div>
 </div>
+
+<div class="container mt-5 px-5 py-5 border">
+    <div class="row">
+        <div class="col">
+            <label for="area" class="form-label">Area</label>
+            <input type="area" class="form-control" id="area" placeholder="area">
+        </div>
+        <div class="col">
+            <label for="category" class="form-label">Category</label>
+            <input type="category" class="form-control" id="Category" placeholder="category">
+        </div>
+        <div class="col">
+            <label for="hashtag" class="form-label">HashTag</label>
+            <input type="hashtag" class="form-control" id="hashtag" placeholder="#hashtag">
+        </div>
+    </div>
+    <div class="container mt-3">
+        <button type="submit" class="btn btn-primary">Search</button>
+        <button type="clear" class="btn btn-outline-secondary">Clear</button>
+    </div>
+</div>
+</div>
 @endsection
