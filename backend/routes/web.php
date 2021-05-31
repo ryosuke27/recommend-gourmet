810fb8c7035622d158cbcb3eaa9e2e42a7ac8adf
{ryosuke27} <opu.7455@gmail.com>
2021-05-31 23:11:30 +0900

    show dish

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -17,3 +17,4 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dish', [App\Http\Controllers\DishController::class, 'show'])->name('dish.show');
