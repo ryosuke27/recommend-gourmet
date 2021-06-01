fff7d24214df68af74d094c1ab425d1144dce195
{ryosuke27} <opu.7455@gmail.com>
2021-06-01 15:00:24 +0900

    show map

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -19,3 +19,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/dish', [App\Http\Controllers\DishController::class, 'show'])->name('dish.show');
 Route::get('/favorite', [App\Http\Controllers\FavoriteController::class, 'show'])->name('favorite.show');
+Route::get('/map', [App\Http\Controllers\MapController::class, 'show'])->name('map.show');
