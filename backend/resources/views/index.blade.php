8bcafb2f8794af0418da7c69a9dd3fb9e1d1f031
{ryosuke27} <opu.7455@gmail.com>
2021-06-01 10:05:58 +0900

    fix

--- backend/resources/views/index.blade.php
+++ backend/resources/views/index.blade.php
@@ -5,15 +5,21 @@
     <div class="row justify-content-center">
         <h2>一瞬で美味しい店を見つけよう</h2>
         <div class="card-group">
-            <div class="card">
-                {!! $embed_html !!}
-            </div>
-            <div class="card">
-                {!! $embed_html !!}
-            </div>
-            <div class="card">
-                {!! $embed_html !!}
-            </div>
+            <a href="{{ route('dish.show') }}">
+                <div class="card">
+                    {!! $embed_html !!}
+                </div>
+            </a>
+            <a href="{{ route('dish.show') }}">
+                <div class="card">
+                    {!! $embed_html !!}
+                </div>
+            </a>
+            <a href="{{ route('dish.show') }}">
+                <div class="card">
+                    {!! $embed_html !!}
+                </div>
+            </a>
         </div>
     </div>
 </div>
