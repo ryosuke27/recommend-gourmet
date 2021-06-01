d988e027c997d22430a5cb83149ad7a44c4d7e27
{ryosuke27} <opu.7455@gmail.com>
2021-06-01 20:37:37 +0900

    fix

--- backend/resources/js/components/HeaderComponent.vue
+++ backend/resources/js/components/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="container-fluid bg-secondary mb-3">
+    <div class="container-fluid bg-dark mb-3">
         <div class="container">
-            <nav class="navbar navbar-secondary">
+            <nav class="navbar navbar-dark">
                 <span class="navbar-brand mb-0 h1">RecommendGourmet</span>
                 <div>
                     <button class="btn btn-success">Favorite</button>
