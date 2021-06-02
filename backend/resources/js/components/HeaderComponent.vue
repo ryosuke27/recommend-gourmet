9bdffe028da927354f1410a0a24ec5a919b2a8e2
{ryosuke27} <opu.7455@gmail.com>
2021-06-02 10:21:58 +0900

    route

--- backend/resources/js/components/HeaderComponent.vue
+++ backend/resources/js/components/HeaderComponent.vue
@@ -2,9 +2,11 @@
     <div class="container-fluid bg-dark mb-3">
         <div class="container">
             <nav class="navbar navbar-dark">
-                <span class="navbar-brand mb-0 h1">RecommendGourmet</span>
+                <router-link v-bind:to="{ name: 'home' }">
+                    <span class="navbar-brand mb-0 h1">RecommendGourmet</span>
+                </router-link>
                 <div>
-                    <router-link v-bind:to="{name: 'favorite'}">
+                    <router-link v-bind:to="{ name: 'favorite' }">
                         <button class="btn btn-success">Favorite</button>
                     </router-link>
                     <button class="btn btn-success">Mypage</button>
