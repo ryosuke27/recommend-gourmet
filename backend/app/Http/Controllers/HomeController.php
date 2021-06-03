2cd87ad9407e478cd027a1784ff7449b7157d6cf
{ryosuke27} <opu.7455@gmail.com>
2021-06-03 17:07:08 +0900

    prefecture json

--- backend/app/Http/Controllers/HomeController.php
+++ backend/app/Http/Controllers/HomeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        // $this->middleware('auth');
     }
 
     /**
@@ -25,6 +25,13 @@ class HomeController extends Controller
     {
 
         $embed_html = '<iframe src="'.url('/image/test1.jpg/?embed').'" style="width:100%;height:100%; border:none;"></iframe>';
+
+        $url = "http://geoapi.heartrails.com/api/json?method=getPrefectures";
+        $json = file_get_contents($url);
+        $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+        $arr = json_decode($json,true);
+        
+        $pref = $arr['response'];
 
         return view('index', [
             "embed_html" => $embed_html
