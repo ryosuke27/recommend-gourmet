91c41fbe45e5ceb4cabbecb3f43cca051bdef16b
{ryosuke27} <opu.7455@gmail.com>
2021-06-05 22:53:24 +0900

    register API

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -18,8 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Auth関連
+Route::post('/register', [Auth\RegisterController::class, 'register'])->name('register');
+Route::post('/login', [Auth\LoginController::class, 'login'])->name('login');
+
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/dish/{dish_id?}', [App\Http\Controllers\DishController::class, 'show'])->name('dish.show');
 Route::get('/favorite', [App\Http\Controllers\FavoriteController::class, 'show'])->name('favorite.show');
+Route::post('/favorite', [App\Http\Controllers\FavoriteController::class, 'add'])->name('favorite.add');
 Route::get('/map', [App\Http\Controllers\MapController::class, 'show'])->name('map.show');
 
