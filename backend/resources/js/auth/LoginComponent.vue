2258e71a1e315863e31ca58ae6408131b46c4890
{ryosuke27} <opu.7455@gmail.com>
2021-06-06 11:57:12 +0900

    registerform

--- backend/resources/js/auth/LoginComponent.vue
+++ backend/resources/js/auth/LoginComponent.vue
@@ -17,9 +17,19 @@
         <div class="panel" v-show="tab === 1">
             <form class="form" @submit.prevent="login">
                 <label for="login-email">Email</label>
-                <input type="text" class="form__item" id="login-email" v-model="loginForm.email" />
+                <input
+                    type="text"
+                    class="form__item"
+                    id="login-email"
+                    v-model="loginForm.email"
+                />
                 <label for="login-password">Password</label>
-                <input type="password" class="form__item" id="login-password" v-model="loginForm.password"/>
+                <input
+                    type="password"
+                    class="form__item"
+                    id="login-password"
+                    v-model="loginForm.password"
+                />
                 <div class="form__button">
                     <button type="submit" class="button button--inverse">
                         login
@@ -27,7 +37,43 @@
                 </div>
             </form>
         </div>
-        <div class="panel" v-show="tab === 2">Register Form</div>
+        <div class="panel" v-show="tab === 2">
+            <form class="form" @submit.prevent="register">
+                <label for="username">Name</label>
+                <input
+                    type="text"
+                    class="form__item"
+                    id="username"
+                    v-model="registerForm.name"
+                />
+                <label for="email">Email</label>
+                <input
+                    type="text"
+                    class="form__item"
+                    id="email"
+                    v-model="registerForm.email"
+                />
+                <label for="password">Password</label>
+                <input
+                    type="password"
+                    class="form__item"
+                    id="password"
+                    v-model="registerForm.password"
+                />
+                <label for="password-confirmation">Password (confirm)</label>
+                <input
+                    type="password"
+                    class="form__item"
+                    id="password-confirmation"
+                    v-model="registerForm.password_confirmation"
+                />
+                <div class="form__button">
+                    <button type="submit" class="button button--inverse">
+                        register
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -39,13 +85,22 @@ export default {
             loginForm: {
                 email: "",
                 password: ""
+            },
+            registerForm: {
+                name: "",
+                email: "",
+                password: "",
+                password_confirmation: ""
             }
         };
     },
     methods: {
-      login () {
-        console.log(this.loginForm)
-      }
+        login() {
+            console.log(this.loginForm);
+        },
+        register() {
+            console.log(this.registerForm);
+        }
     }
 };
 </script>
