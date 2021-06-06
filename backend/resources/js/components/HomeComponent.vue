cb5aef2c98344caa7517bbc81a2b201d7da8e804
{ryosuke27} <opu.7455@gmail.com>
2021-06-06 20:31:37 +0900

    fix

--- backend/resources/js/components/HomeComponent.vue
+++ backend/resources/js/components/HomeComponent.vue
@@ -4,18 +4,21 @@
             <h2>一瞬で美味しい店を見つけよう</h2>
             <div class="card-group">
                 <div class="card">
-                 <div class="row">
-                    <div v-for="(dish, index) in dishes" :key="index">
-                        <router-link
-                            v-bind:to="{
-                                name: 'dish',
-                                params: { dishId: dish.id }
-                            }"
-                        >
-                        <img :src="dish.image_path" style="width: 200px; height: 200px">
-                        </router-link>
+                    <div class="row">
+                        <div v-for="(dish, index) in dishes" :key="index">
+                            <router-link
+                                v-bind:to="{
+                                    name: 'dish',
+                                    params: { dishId: dish.id }
+                                }"
+                            >
+                                <img
+                                    :src="dish.image_path"
+                                    style="width: 200px; height: 200px"
+                                />
+                            </router-link>
+                        </div>
                     </div>
-                 </div>
                 </div>
             </div>
         </div>
