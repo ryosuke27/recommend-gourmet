97fb74a2eb5c3c031529b731296ed70b9a12b023
{ryosuke27} <opu.7455@gmail.com>
2021-06-06 12:53:27 +0900

    util

--- backend/resources/js/bootstrap.js
+++ backend/resources/js/bootstrap.js
@@ -1,3 +1,5 @@
+import { getCookieValue } from './util'
+
 window._ = require('lodash');
 
 /**
@@ -23,6 +25,12 @@ window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
+window.axios.interceptors.request.use(config => {
+    // クッキーからトークンを取り出してヘッダーに添付する
+    config.headers['X-XSRF-TOKEN'] = getCookieValue('XSRF-TOKEN')
+  
+    return config
+})
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
