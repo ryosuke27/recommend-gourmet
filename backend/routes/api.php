dc37b076be1c0fff7e6dbd88d79d3ff65bac4e93
{ryosuke27} <opu.7455@gmail.com>
2021-06-06 20:57:43 +0900

    Auth

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,10 +23,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/register', [App\Http\Controllers\Auth\RegisterController::class, 'register'])->name('register');
 Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login'])->name('login');
 Route::post('/logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
+Route::get('/user', [fn () => Auth::user()])->name('user');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/dish/{dish_id?}', [App\Http\Controllers\DishController::class, 'show'])->name('dish.show');
 Route::get('/favorite', [App\Http\Controllers\FavoriteController::class, 'show'])->name('favorite.show');
 Route::post('/favorite', [App\Http\Controllers\FavoriteController::class, 'add'])->name('favorite.add');
 Route::get('/map', [App\Http\Controllers\MapController::class, 'show'])->name('map.show');
-
