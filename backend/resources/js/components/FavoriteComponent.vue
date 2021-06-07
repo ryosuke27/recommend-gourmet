cb50144f998635196ba99d1d5d82534a5eef5d49
{ryosuke27} <opu.7455@gmail.com>
2021-06-07 12:47:29 +0900

    fix

--- backend/resources/js/components/FavoriteComponent.vue
+++ backend/resources/js/components/FavoriteComponent.vue
@@ -4,7 +4,17 @@
             <div class="card">
                 <div class="row">
                     <div v-for="(dish, index) in dishes" :key="index">
-                       {{ dish.name }}
+                        <router-link
+                            v-bind:to="{
+                                name: 'dish',
+                                params: { dishId: dish.id }
+                            }"
+                        >
+                            <img
+                                :src="dish.image_path"
+                                style="width: 200px; height: 200px"
+                            />
+                        </router-link>
                     </div>
                 </div>
             </div>
