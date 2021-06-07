a044bb0919e2ef6b1e188004a4923d1b8a673e99
{ryosuke27} <opu.7455@gmail.com>
2021-06-07 15:01:20 +0900

    fix

--- backend/resources/js/components/DishComponent.vue
+++ backend/resources/js/components/DishComponent.vue
@@ -2,23 +2,30 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="card mb-3">
-                    <img :src="dish[0].image_path" style="width: 200px; height: 200px">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ dish[0].name }}</h5>
-                        <router-link v-bind:to="{ name: 'map' }">
-                            <button type="button" class="btn btn-success">
-                                Go
-                            </button>
-                        </router-link>
-                        <router-link v-bind:to="{ name: 'favorite.add' }">
-                            <button type="button" class="btn btn-warning" v-on:click="addFavorite(dish[0].id)">
-                                Favorite
-                            </button>
-                        </router-link>
-                        <p class="card-text">
-                            {{ dish[0].description }}
-                        </p>
-                    </div>
+                <img
+                    :src="dish[0].image_path"
+                    style="width: 400px; height: 300px"
+                />
+                <div class="card-body">
+                    <h5 class="card-title">{{ dish[0].name }}</h5>
+                    <router-link v-bind:to="{ name: 'map' }">
+                        <button type="button" class="btn btn-success">
+                            Go
+                        </button>
+                    </router-link>
+                    <router-link v-bind:to="{ name: 'favorite.add' }">
+                        <button
+                            type="button"
+                            class="btn btn-warning"
+                            v-on:click="addFavorite(dish[0].id)"
+                        >
+                            Favorite
+                        </button>
+                    </router-link>
+                    <p class="card-text">
+                        {{ dish[0].description }}
+                    </p>
+                </div>
             </div>
         </div>
     </div>
@@ -27,7 +34,8 @@
 <script>
 export default {
     props: {
-            dishId: String
+        dishId: String,
+        image_path: String
     },
     data: function() {
         return {
@@ -36,13 +44,13 @@ export default {
     },
     methods: {
         getDish() {
-            axios.get('/api/dish/' + this.dishId).then(res => {
+            axios.get("/api/dish/" + this.dishId).then(res => {
                 this.dish = res.data;
             });
         },
         addFavorite(id) {
-            axios.get('/api/dish/' + id).then(res => {
-                this.$router.push({name: 'dish'})
+            axios.get("/api/dish/" + id).then(res => {
+                this.$router.push({ name: "dish" });
             });
         }
     },
