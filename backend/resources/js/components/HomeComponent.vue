4d56dd8d65551788f6ec9f6bbe2bca11f2798790
{ryosuke27} <opu.7455@gmail.com>
2021-06-12 18:53:00 +0900

    search form

--- backend/resources/js/components/HomeComponent.vue
+++ backend/resources/js/components/HomeComponent.vue
@@ -25,51 +25,63 @@
             </div>
         </div>
         <div class="container mt-5 px-5 py-5 border">
-            <div class="row">
-                <div class="col">
-                    <label for="area" class="form-label">Area</label>
-                    <select
-                        type="area"
-                        class="form-control"
-                        id="area"
-                        placeholder="area"
-                    >
-                        <option value="">--Please choose an Area--</option>
-                        <option v-for="area in areas" v-bind:value="area.id">{{
-                            area.name
-                        }}</option>
-                    </select>
+            <form @submit.prevent="search">
+                <div class="row">
+                    <div class="col">
+                        <label for="area" class="form-label">Area</label>
+                        <select
+                            type="area"
+                            class="form-control"
+                            id="area"
+                            placeholder="area"
+                        >
+                            <option value="">--Please choose an Area--</option>
+                            <option
+                                v-for="area in areas"
+                                v-bind:value="area.id"
+                                >{{ area.name }}</option
+                            >
+                        </select>
+                    </div>
+                    <div class="col">
+                        <label for="category" class="form-label"
+                            >Category</label
+                        >
+                        <select
+                            type="category"
+                            class="form-control"
+                            id="category"
+                            placeholder="category"
+                        >
+                            <option value=""
+                                >--Please choose an Category--</option
+                            >
+                            <option
+                                v-for="category in categories"
+                                v-bind:value="category.id"
+                                >{{ category.name }}</option
+                            >
+                        </select>
+                    </div>
+                    <div class="col">
+                        <label for="hashtag" class="form-label">HashTag</label>
+                        <input
+                            type="hashtag"
+                            class="form-control"
+                            id="hashtag"
+                            placeholder="hashtag"
+                        />
+                    </div>
                 </div>
-                <div class="col">
-                    <label for="category" class="form-label">Category</label>
-                    <select
-                        type="category"
-                        class="form-control"
-                        id="category"
-                        placeholder="category"
-                    >
-                        <option value="">--Please choose an Category--</option>
-                        <option v-for="category in categories" v-bind:value="category.id">{{
-                            category.name
-                        }}</option>
-                    </select>
+                <div class="container mt-3">
+                    <button type="submit" class="btn btn-primary">
+                        Search
+                    </button>
+                    <button type="clear" class="btn btn-outline-secondary">
+                        Clear
+                    </button>
                 </div>
-                <div class="col">
-                    <label for="hashtag" class="form-label">HashTag</label>
-                    <input
-                        type="hashtag"
-                        class="form-control"
-                        id="hashtag"
-                        placeholder="hashtag"
-                    />
-                </div>
-            </div>
-            <div class="container mt-3">
-                <button type="submit" class="btn btn-primary">Search</button>
-                <button type="clear" class="btn btn-outline-secondary">
-                    Clear
-                </button>
-            </div>
+            </form>
         </div>
     </div>
 </template>
