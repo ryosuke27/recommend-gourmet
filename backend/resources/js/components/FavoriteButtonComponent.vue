f3f5e3936fb3df32002143ba8e8904401ab73450
{ryosuke27} <opu.7455@gmail.com>
2021-06-12 15:25:44 +0900

    fix

--- backend/resources/js/components/FavoriteButtonComponent.vue
+++ backend/resources/js/components/FavoriteButtonComponent.vue
@@ -23,6 +23,8 @@ export default {
             //     alert("いいね機能を使うにはログインしてください。");
             //     return false;
             // }
+
+            // liked_by_userで条件分岐させる
             if (this.dish.liked_by_user) {
                 this.unfavorite();
             } else {
