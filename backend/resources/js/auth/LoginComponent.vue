08076e27220cc2fd6efeb562e1f268054465d812
{ryosuke27} <opu.7455@gmail.com>
2021-08-14 20:14:38 +0900

    error message style register

--- backend/resources/js/auth/LoginComponent.vue
+++ backend/resources/js/auth/LoginComponent.vue
@@ -63,14 +63,14 @@
       <div class="card-header">Register</div>
       <form class="form card-body" @submit.prevent="register">
         <div v-if="registerErrors" class="errors">
-          <ul v-if="registerErrors.name">
-            <li v-for="msg in registerErrors.name" :key="msg">{{ msg }}</li>
+          <ul class="list-unstyled border border-danger" v-if="registerErrors.name">
+            <li class="text-danger" v-for="msg in registerErrors.name" :key="msg">{{ msg }}</li>
           </ul>
-          <ul v-if="registerErrors.email">
-            <li v-for="msg in registerErrors.email" :key="msg">{{ msg }}</li>
+          <ul class="list-unstyled border border-danger" v-if="registerErrors.email">
+            <li class="text-danger" v-for="msg in registerErrors.email" :key="msg">{{ msg }}</li>
           </ul>
-          <ul v-if="registerErrors.password">
-            <li v-for="msg in registerErrors.password" :key="msg">{{ msg }}</li>
+          <ul class="list-unstyled border border-danger" v-if="registerErrors.password">
+            <li class="text-danger" v-for="msg in registerErrors.password" :key="msg">{{ msg }}</li>
           </ul>
         </div>
         <div class="mb-3">
